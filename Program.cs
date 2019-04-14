d72bc3e3c018103b1fdd31a9ca63064b4e6ec9ac
Alejandro P <alejandroparparcen@yahoo.com>
2019-04-13 22:02:06 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,21 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ConsoleCards;
 
-namespace Excercise4
+namespace Excercise4C2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the first altitude in feets:");
-            int alt1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter the second altitude:");
-            int alt2 = int.Parse(Console.ReadLine());
-            int altDif = alt2 - alt1;
-            Console.WriteLine(altDif + " Feet");
+            Deck deck = new Deck();
+            //prompts for values
+            Console.WriteLine("Gimmie values and i'll fill the between");
+            Console.WriteLine("Value 1:");
+            int firstN = int.Parse(Console.ReadLine());
+            Console.WriteLine("Value 2:");
+            int secondN = int.Parse(Console.ReadLine());
+            //checks if value is higher or lower 
+            if (firstN >= secondN)
+            {
+                for(;firstN >= secondN; firstN--)
+                {
+                    //prints value if it's higher or equal
+                    Console.WriteLine(firstN);
+                }
+            }
+            else
+            {
+                for (; firstN <= secondN; firstN++)
+                {
+                    //prints value if it's higher or equal
+                    Console.WriteLine(firstN);
+                }
+            }
             Console.ReadLine();
-
+            List<Card> Hand = new List<Card>();
+            deck.Shuffle();
+            for(int i = 0; i < 5; i++)
+            {
+                Hand.Add(deck.TakeTopCard());
+            }
+            for(int i = 0; i < Hand.Count; i++)
+            {
+                Hand[i].FlipOver();
+            }
+            foreach(Card card in Hand)
+            {
+                card.Print();
+            }
+            Console.ReadLine();
         }
     }
 }
