3f79e08a2a6b5af42e3bb54cb376a202e98c92cd
Alejandro P <alejandroparparcen@yahoo.com>
2019-04-13 22:00:05 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first altitude in feets:");
+            int alt1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second altitude:");
+            int alt2 = int.Parse(Console.ReadLine());
+            int altDif = alt2 - alt1;
+            Console.WriteLine(altDif + " Feet");
+            Console.ReadLine();
+
+        }
+    }
+}
